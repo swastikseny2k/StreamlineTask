93688a438bc5450ca9884259f70090f8f67648ff
swastikseny2k <swastikseny2k@gmail.com>
2017-11-11 17:53:16 -0500

    aspect modified

--- src/main/java/com/sen/streamlinetask/aspect/LoggingAspect.java
+++ src/main/java/com/sen/streamlinetask/aspect/LoggingAspect.java
@@ -2,7 +2,10 @@ package com.sen.streamlinetask.aspect;
 
 import org.apache.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -14,13 +17,36 @@ public class LoggingAspect {
 	
 	private Logger logger = Logger.getLogger(getClass().getName());
 	
-	@Before("execution(* com.sen.streamlinetask.*.*(..))")
+	@Before("execution(* com.sen.streamlinetask..*.*(..))")
 	public void traceEnter(JoinPoint jp) {
 		logger.info("Entering method " + jp.getSignature().getName() + " of class "+ jp.getSignature().getClass());
 	}
 	
-	@After("execution(* com.sen.streamlinetask.*.*(..))")
+	@After("execution(* com.sen.streamlinetask..*.*(..))")
 	public void traceExit(JoinPoint jp) {
 		logger.info("Exiting method " + jp.getSignature().getName() + " of class "+ jp.getSignature().getClass());
+	}
+	
+	/*@Around("execution(* com.sen.streamlinetask..*.*(..))")
+	public Object logException(ProceedingJoinPoint jp) {
+		Object result = null;
+		try {
+			result = jp.proceed();
+			
+		} catch(Exception ex) {
+			
+			logger.error("Exiting method " + jp.getSignature().getName() + " of class "+ jp.getSignature().getClass());
+		} catch (Throwable e) {
+
+			logger.error("Exiting method " + jp.getSignature().getName() + " of class "+ jp.getSignature().getClass());
+		}
+		return result;
+	}*/
+	
+	@AfterThrowing(pointcut="execution(* com.sen.streamlinetask..*.*(..))", throwing="ex" )
+	public void logException(Exception ex) {
+
+		logger.error("Error has occurred");
+		logger.error(ex);
 	}
 }
